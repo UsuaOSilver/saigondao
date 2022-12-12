1173808081b7ed7f30ad586d0bf7c9eedf5078cc
UsuaOSilver <anguyen8063@student.nocccd.edu>
2022-12-12 12:14:43 +0700

    testing both scripts - cant verify, need longer confirmation time

--- scripts/deploy-token.js
+++ scripts/deploy-token.js
@@ -5,23 +5,26 @@ const {
 } = require("../helper-hardhat-config")
 const { verify } = require("../helper-functions")
 
+const tokenName = "Ngaen"
+const tokenSymbol = "SGN"
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const ngaen = await deploy("Ngaen", {
         from: deployer,
-        args: [INITIAL_SUPPLY],
+        args: [INITIAL_SUPPLY, tokenName, tokenSymbol],
         log: true,
         // on live network
         waitConfirmations: network.config.blockConfirmations || 1,
     })
-    log(`Ngaen deployed at $${ngaen.address}`)
+    log(`Ngaen deployed at ${ngaen.address}`)
     
     if (
         !developmentChains.includes(network.name) &&
         process.env.ETHERSCAN_API_KEY
     ) {
-        await verify(ngaen.address, [INITIAL_SUPPLY])
+        await verify(ngaen.address, [INITIAL_SUPPLY, tokenName, tokenSymbol])
     }
 }
 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,27 +5,37 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const {
+    developmentChains,
+    INITIAL_SUPPLY,
+} = require("../helper-hardhat-config")
+const { verify } = require("../helper-functions")
+
+const tokenName = "Ngaen"
+const tokenSymbol = "SGN"
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const Ngaen = await hre.ethers.getContractFactory("Ngaen");
+  const ngaen = await Ngaen.deploy(INITIAL_SUPPLY, tokenName, tokenSymbol);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await ngaen.deployed();
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log(`Ngaen deployed at ${ngaen.address}`)
+    
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(ngaen.address, [INITIAL_SUPPLY, tokenName, tokenSymbol])
+    }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
